7c6e2a10d5f213f16aba61ee385d1ba6696ad13c
anishshirote54-source <anishshirote54@gmail.com>
2025-12-20 17:02:25 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,7 @@
+a = "soham"
+age = 18
+location = "mumbai"
+
+print("the name of the student is -", a)
+print("the age of the student is -", age)
+print("the place of residence od the student is -", location)
