a19b90cc7d42ebc4589d87292e8941d33d3170d5
anishshirote54-source <anishshirote54@gmail.com>
2025-12-25 21:34:07 +0530

    Add files via upload

---
+++ setquestion.py
@@ -0,0 +1,5 @@
+a = {"c++", "java", "python", "python", "javascrpit", "java", "python", "java", "c++", "C"}
+
+#number of clases required per subject 
+class_required = len(a)
+print("the number of classes required are -", class_required)
